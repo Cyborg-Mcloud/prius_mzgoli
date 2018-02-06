c2bb3f38f60d8a130761c84c22d984136866f1de
Cyborg-Mcloud <uabashidze@gmail.com>
2018-02-06 10:54:31 +0400

    vcvc
    eee

--- maps.js
+++ maps.js
@@ -111,45 +111,7 @@ function initMap()
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
     var autocomplete = new google.maps.places.Autocomplete(input);
 
-    autocomplete.bindTo('bounds', map);
-    autocomplete.addListener('place_changed', function () {
-        // infoWindow.close();
-        tempMarker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-        }
-
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17); // Why 17? Because it looks good.
-        }
-
-        //tempMarker.setPosition(place.geometry.location);
-		geocodeOnClick({latLng: place.geometry.location})
-
-        // geocodePosition(place.geometry.location, currentField);
-        tempMarker.setVisible(true);
-        var address = '';
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
-
-        // infoWindowContent.children['place-icon'].src = place.icon;
-        // infoWindowContent.children['place-name'].textContent = place.name;
-        // infoWindowContent.children['place-address'].textContent = address;
-        infoWindow.open(map, tempMarker);
-    });
+   
 
 }
 
